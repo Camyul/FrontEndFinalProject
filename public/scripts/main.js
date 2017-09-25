4408016a76dff8f56ac05b3308156679ce3223c6
Camyul <vankata.lt@outlook.com>
2017-09-25 14:42:10 +0300

    add scripts in index.html

---
+++ public/scripts/main.js
@@ -0,0 +1,9 @@
+let div = document.createElement('div');
+let inputButton = document.createElement('button');
+
+div.innerHTML = 'Final Project';
+inputButton.className += ' btn btn-success';
+inputButton.innerText = 'Test';
+
+document.body.appendChild(div);
+document.body.appendChild(inputButton);
