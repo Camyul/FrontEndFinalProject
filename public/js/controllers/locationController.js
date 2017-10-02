ff3775742ac9db4bab627c84f440693285582353
Camyul <vankata.lt@outlook.com>
2017-10-02 14:14:46 +0300

    After-header bar complete

--- public/js/controllers/locationController.js
+++ public/js/controllers/locationController.js
@@ -11,6 +11,29 @@ class LocationController {
                 // console.log('location controller');
                 // console.log(data); // TODO: да сменя getMenu() с or remove dataService
                 $('#logo2').addClass('hide');
+                const $title = $('<h1>');
+                $title.text('store location');
+                const $h5 = $('<h5>');
+                $h5.text('Neapolitan pizza and grandmother\'s kitchen.');
+                $('.header-info').append($title);
+                $('.header-info').append($h5);
+
+                const $h4 = $('<h4>');
+                $h4.text('Home >> Localization');
+                $('.info-left').append($h4);
+
+                // After header bar - start
+                const $input = $('<input>');
+                $input.attr('placeholder', 'Search this website');
+                $input.addClass('form-control search-right');
+                const $searchButton = $('<button>');
+                $searchButton.text('Search');
+                $searchButton.addClass('btn btn-success search-right');
+
+                $('#search-right').append($input);
+                $('#search-right').append($searchButton);
+                // After header bar - end
+
                 const html = template();
                 $container.html(html);
             })
