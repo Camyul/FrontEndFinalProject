a6f8157a3d800b825533df77f09440bc70a93865
Camyul <vankata.lt@outlook.com>
2017-10-06 14:21:41 +0300

    change port name

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 app.use('/libs', express.static('node_modules'));
 
-const port = process.env.port || 3013;
+const port = process.env.PORT || 3013;
 
 app.listen(port, () => {
     console.log('Server is running at http://localhost:' + port);
